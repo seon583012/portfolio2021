8a737fae7deaa9cb61cb91dd84e44fcc21fd6928
seon583012 <seon1994@naver.com>
2021-06-21 13:40:32 +0900

    q

--- vendor/visual_pop.js
+++ vendor/visual_pop.js
@@ -1,30 +1,32 @@
 $(function(){
     
-    
-    $(".single-img.card>.opacity ul li").click(function(){
+    $(".single-img.v1>.opacity .pop_btn li").click(function(){
         
-        $(".popup_container.card").fadeIn();
+        $(".popup_container.v1").fadeIn();
         $("body").css('overflow','hidden');
     });
     
+    $(".single-img.v2>.opacity .pop_btn li").click(function(){
+        
+        $(".popup_container.v2").fadeIn();
+        $("body").css('overflow','hidden');
+    });
+   
+    
+    $(".single-img.v3>.opacity .pop_btn li").click(function(){
+        
+        $(".popup_container.v3").fadeIn();
+        $("body").css('overflow','hidden');
+    });
+    
+    
+     
     $(".popup_close").click(function(){
         
         $(".popup_container").fadeOut();
         $("body").css('overflow','inherit');
     })
     
-    
-    $(".single-img.landing>.opacity ul li").click(function(){
-        
-        $(".popup_container.landing").fadeIn();
-        $("body").css('overflow','hidden');
-    });
-    
-    $(".popup_close").click(function(){
-        
-        $(".popup_container").fadeOut();
-        $("body").css('overflow','inherit');
-    })
     
     
 });
